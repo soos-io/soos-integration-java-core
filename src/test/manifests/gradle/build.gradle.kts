e1a5d9184f35b6d0b80a0e09f90be15068fc991b
SOOS-JAlvarez <jalvarez@soos.io>
2022-03-15 15:53:31 -0300

    PA-4174 Added unit tests

---
+++ src/test/manifests/gradle/build.gradle.kts
@@ -0,0 +1,16 @@
+﻿rootProject.name = 'SoosApp'
+version = "0.1"
+
+plugins {
+    id("demo.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-text:1.9")
+    implementation("org.apache.logging.log4j:log4j-core:2.16.0")
+    testImplementation("junit:junit:4.+")
+}
+
+application {
+    mainClass.set("demo.app.AppKt")
+}
