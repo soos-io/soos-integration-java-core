06d99b4fa8d71895da040b4e05551c6481d7f557
SOOS-JAlvarez <jalvarez@soos.io>
2024-02-16 15:35:56 -0300

    added enums

---
+++ src/main/java/io/soos/integration/Enums.java
@@ -0,0 +1,13 @@
+package io.soos.integration;
+
+public class Enums {
+
+    public enum LogLevel {
+        DEBUG, INFO, WARN, FAIL, ERROR
+    }
+
+    public enum OnFailure {
+        FAIL_THE_BUILD, CONTINUE_ON_FAILURE
+    }
+    
+}
